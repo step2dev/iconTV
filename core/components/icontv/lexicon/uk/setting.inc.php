2aff5dfcb2918c14fe5d9c44cb047d8cf4ed5dee
Yura Finiv <CrazyBoy49z@users.noreply.github.com>
2020-02-26 16:51:19 +0200

    New translations setting.inc.php (Ukrainian)

---
+++ core/components/icontv/lexicon/uk/setting.inc.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Setting Lexicon Entries for IconTV
+ *
+ * @package icontv
+ * @subpackage lexicon
+ */
+$_lang['area_icontv.path'] = 'Шлях';
+$_lang['area_icontv.main'] = 'Загальні';
+$_lang['area_icontv.setting'] = 'Налаштування';
+$_lang['setting_icontv.config.svg'] = 'Шлях до налаштувань';
+$_lang['setting_icontv.config.svg_desc'] = 'Шлях до налаштувань';
+$_lang['setting_icontv.path.svg'] = 'Шлях до парсера svg';
+$_lang['setting_icontv.path.svg_desc'] = 'Шлях до парсера svg';
+$_lang['setting_icontv.auto.close'] = 'Автозакриття';
+$_lang['setting_icontv.auto.close_desc'] = 'Незалежно від того, чи автоматично закривати програму вибору піктограм при натисканні в іншому місці.';
+$_lang['setting_icontv.theme'] = 'Тема, яку потрібно використовувати';
+$_lang['setting_icontv.theme_desc'] = "fip-grey: Тема за замовчуванням. Здійснює вибір красивих значків сірого кольору. <br />
+fip-darkgrey: більш глибокий відтінок сірої теми з градієнтами CSS3, закругленими кутами та тінями. <br />
+fip-bootstrap: приємна тема вибору іконок, сумісна з Bootstrap. <br />
+fip-inverted: перевернута колірна схема.";
+$_lang['setting_icontv.icons.per.page'] = 'Кількість елементів на сторінці';
+$_lang['setting_icontv.icons.per.page_desc'] = 'Кількість піктограм на сторінці в засобі вибору значків. Для кращого вигляду завжди надайте число, кратне 5.';
+$_lang['setting_icontv.empty.icon'] = 'Показати порожній значок';
+$_lang['setting_icontv.destroy.api'] = 'Показати кнопку знищення';
+$_lang['setting_icontv.destroy.api_desc'] = '';
