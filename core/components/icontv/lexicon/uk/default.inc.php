87d6465a90c74302c10bbafe967b54d81cea61d0
Yura Finiv <CrazyBoy49z@users.noreply.github.com>
2020-02-26 16:51:18 +0200

    New translations default.inc.php (Ukrainian)

---
+++ core/components/icontv/lexicon/uk/default.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Default Lexicon Entries for IconTV
+ *
+ * @package icontv
+ * @subpackage lexicon
+ */
+
+$_lang['icons'] = 'Іконки';
+$_lang['iconstv.desc'] = 'Виберіть підтримку піктограм';
+$_lang['iconstv.noSearch'] = 'Вимкнути панель пошуку';
+$_lang['iconstv.preview'] = 'Preview';
