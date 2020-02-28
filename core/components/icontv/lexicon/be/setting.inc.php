253b69a93075bc63027cb70e26374d63e81b6aef
Yura Finiv <CrazyBoy49z@users.noreply.github.com>
2020-02-28 17:14:14 +0200

    New translations setting.inc.php (Belarusian)

--- core/components/icontv/lexicon/be/setting.inc.php
+++ core/components/icontv/lexicon/be/setting.inc.php
@@ -6,21 +6,22 @@
  * @subpackage lexicon
  */
 $_lang['area_icontv.path'] = 'Шлях';
-$_lang['area_icontv.main'] = 'General';
-$_lang['area_icontv.setting'] = 'Configuration';
-$_lang['setting_icontv.config.svg'] = 'Config path';
-$_lang['setting_icontv.config.svg_desc'] = 'Config path';
-$_lang['setting_icontv.path.svg'] = 'Path for parser svg';
-$_lang['setting_icontv.path.svg_desc'] = 'Path for parser svg';
-$_lang['setting_icontv.auto.close'] = 'Automatically close';
-$_lang['setting_icontv.auto.close_desc'] = 'Whether or not to close the icon picker automatically when clicked elsewhere.';
-$_lang['setting_icontv.theme'] = 'The theme to be used';
-$_lang['setting_icontv.theme_desc'] = "fip-grey: The default theme. Renders beautiful grey colored icon picker. <br />
-fip-darkgrey: A deeper shade of the grey theme with CSS3 gradients, rounded corners and shadows. <br />
-fip-bootstrap: A nice icon picker theme compatible with Bootstrap. <br />
-fip-inverted: Inverted color scheme.";
-$_lang['setting_icontv.icons.per.page'] = 'Number of icons per page';
-$_lang['setting_icontv.icons.per.page_desc'] = 'Number of icons per page on the icon picker. For better appearance always provide a number in multiple of 5.';
-$_lang['setting_icontv.empty.icon'] = 'Show empty icon';
-$_lang['setting_icontv.destroy.api'] = 'Show destroy button';
+$_lang['area_icontv.main'] = 'Галоўныя';
+$_lang['area_icontv.setting'] = 'Канфігурацыя';
+$_lang['setting_icontv.config.svg'] = 'Наладзьце шлях';
+$_lang['setting_icontv.config.svg_desc'] = 'Наладзьце шлях';
+$_lang['setting_icontv.path.svg'] = 'Шлях для разбору svg';
+$_lang['setting_icontv.path.svg_desc'] = 'Шлях для разбору svg';
+$_lang['setting_icontv.auto.close'] = 'Аўтаматычнае закрыццё';
+$_lang['setting_icontv.auto.close_desc'] = 'Незалежна ад таго, патрэбна лі аўтаматычна зачыняць сродкі выбару іконак, пры націску ў іншым месцы.';
+$_lang['setting_icontv.theme'] = 'Тэма, якая будзе выкарыстоўвацца';
+$_lang['setting_icontv.theme_desc'] = "fip-grey: Тэма па змаўчанні.
+ Здаецца прыгожы падборшчык значкоў шэрага колеру. <br />
+fip-darkgrey: больш глыбокі адценне шэрай тэмы з градыентамі CSS3, закругленымі кутамі і ценямі. <br />
+fip-bootstrap: Прыгожая тэма выбару абразкоў, сумяшчальная з Bootstrap. <br />
+fip-inverted: перавернутая каляровая схема.";
+$_lang['setting_icontv.icons.per.page'] = 'Колькасць значкоў на старонцы';
+$_lang['setting_icontv.icons.per.page_desc'] = 'Колькасць іконак на старонцы ў сродках выбару іконак. Для лепшага вонкавага выгляду заўсёды указвайце лік, кратнае 5.';
+$_lang['setting_icontv.empty.icon'] = 'Паказваць пустую іконку';
+$_lang['setting_icontv.destroy.api'] = 'Паказваць кнопку знішчэння';
 $_lang['setting_icontv.destroy.api_desc'] = '';
